ff8a8b0982645d8648e91d259b8e2024a98722db
Marcin-99 <MarcinAszyk@o2.pl>
2020-07-21 16:34:39 +0200

    Implemented functionality for writing images with a proper name into the output folder.

--- main.scala
+++ main.scala
@@ -1,43 +1,46 @@
  import javax.imageio.ImageIO
  import java.io.File
  import java.awt.image.BufferedImage
- import java.io.File
 
 
 object main {
-	def main(args: Array[String]) {
-
-		def getAverageLuminosity(image: BufferedImage) : Float = {
-			var darknessRatio: Float = 0;
-			val numOfPixels = image.getWidth()*image.getHeight()
-
-			for (i <- 0 to image.getHeight() - 1) {
-				for (j <- 0 to image.getWidth() - 1) {
-					val color = image.getRGB(j, i)
-					val red = (color & 0xff0000) / 65536
-					val green = (color & 0xff00) / 256
-					val blue = (color & 0xff)
-					darknessRatio += (100 - ((0.2126*red + 0.7152*green + 0.0722*blue)/2.55)).toFloat
-				}
+	def getAverageLuminosity(image: BufferedImage) : Float = {
+		val numOfPixels: Int = image.getWidth()*image.getHeight()
+		var darknessRatio: Float = 0;
+		var color, red, green, blue: Int = 0;
+		for (i <- 0 to image.getHeight() - 1) {
+			for (j <- 0 to image.getWidth() - 1) {
+				color = image.getRGB(j, i)
+				red = (color & 0xff0000) / 65536
+				green = (color & 0xff00) / 256
+				blue = (color & 0xff)
+				darknessRatio += (100 - ((0.2126*red + 0.7152*green + 0.0722*blue)/2.55)).toFloat
 			}
-
-			return darknessRatio/numOfPixels
 		}
+		return darknessRatio/numOfPixels
+	}
 
+	def getListOfFiles(dir: String) : List[File] = {
+		val file = new File(dir)
+		if (file.exists && file.isDirectory) file.listFiles.filter(_.isFile).toList
+		else List[File]()
+	}
+	
+	def buildOutputFile(file: File, averageLuminosity: Float, extension: String) : File = {
+		val name = file.getName().replaceFirst("[.][^.]+$", "")
+		val darkOrBright = if (averageLuminosity > 72) "dark" else "bright"
+		val newFile = new File("output/" + name + "_" + darkOrBright + "_" + averageLuminosity.toInt + "." + extension)
+		return newFile
+	}
 
-		def getListOfFiles(dir: String):List[File] = {
-		    val d = new File(dir)
-		    if (d.exists && d.isDirectory) d.listFiles.filter(_.isFile).toList
-		    else List[File]()
-		}
-
-
+	def main(args: Array[String]) {
 		val files = getListOfFiles("input/")
 		for (file <- files) {
-			var image = ImageIO.read(file)
-			var averageLuminosity = getAverageLuminosity(image)
-			println(file + ": " + averageLuminosity)
+			var image: BufferedImage = ImageIO.read(file)
+			var averageLuminosity: Float = getAverageLuminosity(image)
+			var extension: String = file.getName().toString.split("\\.").last
+			var outputFile = buildOutputFile(file, averageLuminosity, extension)
+			ImageIO.write(image, extension, outputFile)
 		}
-
 	}
 }
