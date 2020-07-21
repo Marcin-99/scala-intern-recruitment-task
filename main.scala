e15dbbbd0e4867b3e134bf82da0ba38368b0c636
Marcin-99 <MarcinAszyk@o2.pl>
2020-07-21 13:57:17 +0200

    Implemented proper formula for darkness ratio and listed all files from input directory.

--- main.scala
+++ main.scala
@@ -1,12 +1,14 @@
  import javax.imageio.ImageIO
  import java.io.File
  import java.awt.image.BufferedImage
+ import java.io.File
 
 
 object main {
 	def main(args: Array[String]) {
+
 		def getAverageLuminosity(image: BufferedImage) : Float = {
-			var luminosity: Float = 0;
+			var darknessRatio: Float = 0;
 			val numOfPixels = image.getWidth()*image.getHeight()
 
 			for (i <- 0 to image.getHeight() - 1) {
@@ -15,15 +17,27 @@ object main {
 					val red = (color & 0xff0000) / 65536
 					val green = (color & 0xff00) / 256
 					val blue = (color & 0xff)
-					luminosity += (0.2126*red + 0.7152*green + 0.0722*blue).toFloat
+					darknessRatio += (100 - ((0.2126*red + 0.7152*green + 0.0722*blue)/2.55)).toFloat
 				}
 			}
 
-			return luminosity/numOfPixels
+			return darknessRatio/numOfPixels
 		}
 
-		val image = ImageIO.read(new File("a.jpg"))
-		var averageLuminosity = getAverageLuminosity(image)
-		println(averageLuminosity)
+
+		def getListOfFiles(dir: String):List[File] = {
+		    val d = new File(dir)
+		    if (d.exists && d.isDirectory) d.listFiles.filter(_.isFile).toList
+		    else List[File]()
+		}
+
+
+		val files = getListOfFiles("input/")
+		for (file <- files) {
+			var image = ImageIO.read(file)
+			var averageLuminosity = getAverageLuminosity(image)
+			println(file + ": " + averageLuminosity)
+		}
+
 	}
 }
